9b8188e67c1799e12b9764e23d427fba22755c8f
Arterixs <artemontugg@gmail.com>
2023-06-14 02:35:12 +0300

    init: start task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import { writeFile } from "node:fs/promises";
+import { getPathCurrent } from "../helpers/index.js";
+
 const create = async () => {
-    // Write your code here 
+  const path = getPathCurrent(import.meta.url, "files", "fresh.txt");
+  writeFile(path, "I am fresh and young", { flag: "wx" }).catch((err) => {
+    err.message = "FS operation failed";
+    console.log(err);
+    throw new Error(err);
+  });
 };
 
 await create();
---
+++ src/helpers/index.js
@@ -0,0 +1,7 @@
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
+
+export const getPathCurrent = (url, ...args) => {
+  const _filename = fileURLToPath(url);
+  return path.join(dirname(_filename), ...args);
+};
