1196590aa7d906ea8d5d60765ee5df3e5daf19e3
Arterixs <artemontugg@gmail.com>
2023-06-14 14:56:17 +0300

    feat: add logic copy mkdir and file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { mkdir, readdir, copyFile } from "node:fs/promises";
+import { getPathCurrent } from "../helpers/index.js";
+
 const copy = async () => {
-    // Write your code here 
+  const pathCopyMcdir = getPathCurrent(import.meta.url, "files_copy");
+  const pathOriginMcdir = getPathCurrent(import.meta.url, "files");
+  try {
+    const files = await readdir(pathOriginMcdir).catch((error) => {
+      error.message = "FS operation failed";
+      throw error;
+    });
+    await mkdir(pathCopyMcdir, { recursive: false }).catch((error) => {
+      error.message = "FS operation failed";
+      throw error;
+    });
+    files.forEach(async (item) => {
+      await copyFile(`${pathOriginMcdir}/${item}`, `${pathCopyMcdir}/${item}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -4,9 +4,9 @@ import { getPathCurrent } from "../helpers/index.js";
 const create = async () => {
   const path = getPathCurrent(import.meta.url, "files", "fresh.txt");
   try {
-    writeFile(path, "I am fresh and young", { flag: "wx" }).catch((err) => {
-      err.message = "FS operation failed";
-      throw err;
+    writeFile(path, "I am fresh and young", { flag: "wx" }).catch((error) => {
+      error.message = "FS operation failed";
+      throw error;
     });
   } catch (err) {
     console.error(err);
