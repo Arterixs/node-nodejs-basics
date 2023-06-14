b89ff00b9de2d900be5fc0f219492f17ff495fc3
Arterixs <artemontugg@gmail.com>
2023-06-14 13:31:47 +0300

    refactor: update throw error in create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -3,11 +3,14 @@ import { getPathCurrent } from "../helpers/index.js";
 
 const create = async () => {
   const path = getPathCurrent(import.meta.url, "files", "fresh.txt");
-  writeFile(path, "I am fresh and young", { flag: "wx" }).catch((err) => {
-    err.message = "FS operation failed";
-    console.log(err);
-    throw new Error(err);
-  });
+  try {
+    writeFile(path, "I am fresh and young", { flag: "wx" }).catch((err) => {
+      err.message = "FS operation failed";
+      throw err;
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await create();
