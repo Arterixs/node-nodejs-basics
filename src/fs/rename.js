dae9dcf5de612bc78ffbffba49cc392fd4a25812
Arterixs <artemontugg@gmail.com>
2023-06-14 15:25:32 +0300

    feat: add rename task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import { rename as renamed } from "node:fs/promises";
+import { getPathCurrent, triggerErrorFs } from "../helpers/index.js";
+
 const rename = async () => {
-    // Write your code here 
+  const pathOldFile = getPathCurrent(
+    import.meta.url,
+    "files",
+    "wrongFilename.txt"
+  );
+  const pathNewFile = getPathCurrent(
+    import.meta.url,
+    "files",
+    "properFilename.md"
+  );
+  try {
+    renamed(pathOldFile, pathNewFile).catch((err) => triggerErrorFs(err));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await rename();
