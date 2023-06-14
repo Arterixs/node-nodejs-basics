37a745dd18973d69fbf682245113e536cb646ddf
Arterixs <artemontugg@gmail.com>
2023-06-14 15:46:54 +0300

    feat: add list task fs

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { readdir } from "node:fs/promises";
+import { getPathCurrent, triggerErrorFs } from "../helpers/index.js";
+
 const list = async () => {
-    // Write your code here 
+  const pathMcdir = getPathCurrent(import.meta.url, "files");
+  try {
+    readdir(pathMcdir)
+      .then((result) => console.log(result))
+      .catch((error) => triggerErrorFs(error));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await list();
