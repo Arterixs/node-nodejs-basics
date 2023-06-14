8bc1ea677f821cd3fa0d82e883c8ac94a3c16d08
Arterixs <artemontugg@gmail.com>
2023-06-14 16:58:39 +0300

    feat: add args cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
+import { argv } from "node:process";
+
 const parseArgs = () => {
-    // Write your code here 
+  const arrArgv = argv.slice(2);
+  const result = arrArgv
+    .reduce((acc, item, indx, arr) => {
+      if (!item.startsWith("--")) return acc;
+      const arrSm = [];
+      const newItem = item.slice(2);
+      arrSm.push(newItem, arr[indx + 1]);
+      acc.push(arrSm);
+      return acc;
+    }, [])
+    .map((item) => item.join(" is "))
+    .join(", ");
+  console.log(result);
 };
 
 parseArgs();
