4d705c6f47082949756edb985769f883ee649bd5
Arterixs <artemontugg@gmail.com>
2023-06-14 15:56:10 +0300

    feat: add read fs task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { readFile } from "node:fs/promises";
+import { getPathCurrent, triggerErrorFs } from "../helpers/index.js";
+
 const read = async () => {
-    // Write your code here 
+  const pathMcdir = getPathCurrent(import.meta.url, "files", "fileToRead.txt");
+  try {
+    readFile(pathMcdir, "UTF-8")
+      .then((result) => console.log(result))
+      .catch((err) => triggerErrorFs(err));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await read();
