35675cb51ed93b1dd123eb4e3f8ff9d539681522
Arterixs <artemontugg@gmail.com>
2023-06-15 17:50:55 +0300

    feat: add write task streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { createWriteStream } from "node:fs";
+import { stdin } from "node:process";
+import { getPathCurrent } from "../helpers/index.js";
+
 const write = async () => {
-    // Write your code here 
+  const pathWriteFile = getPathCurrent(
+    import.meta.url,
+    "files",
+    "fileToWrite.txt"
+  );
+  const writeFile = createWriteStream(pathWriteFile);
+  stdin.on("data", (data) => {
+    writeFile.write(data);
+  });
 };
 
 await write();
