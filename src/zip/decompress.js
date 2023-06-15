ddd2b1bfe84d7aaab46e8d2ab71d219fce97bbe4
Arterixs <artemontugg@gmail.com>
2023-06-15 20:41:44 +0300

    feat: add decompress task zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createUnzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import { getPathCurrent } from "../helpers/index.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const pathNewFile = getPathCurrent(
+    import.meta.url,
+    "files",
+    "fileToCompress.txt"
+  );
+  const pathGzip = getPathCurrent(import.meta.url, "files", "archive.gz");
+  const readFile = createReadStream(pathGzip);
+  const unzipFile = createUnzip();
+  const writeFile = createWriteStream(pathNewFile);
+
+  pipeline(readFile, unzipFile, writeFile);
 };
 
 await decompress();
