6ac832774009ca22c58350b3ab8139dd6dceccdb
Arterixs <artemontugg@gmail.com>
2023-06-15 19:02:34 +0300

    feat: add transform task streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from "node:stream";
+import { pipeline } from "node:stream/promises";
+import { stdin, stdout } from "node:process";
+
 const transform = async () => {
-    // Write your code here 
+  const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+      const cleanString = chunk.toString().trim();
+      const reverseString = cleanString.split("").reverse().join("");
+      const finalResult = `${reverseString}\n`;
+      callback(null, finalResult);
+    },
+  });
+  await pipeline(stdin, reverseTransform, stdout);
 };
 
 await transform();
