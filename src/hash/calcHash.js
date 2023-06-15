c32b9cd5c8bc4cfb5d9d39b6f789b17a51fb4baa
Arterixs <artemontugg@gmail.com>
2023-06-15 14:49:16 +0300

    feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createHash } from "node:crypto";
+import { readFile } from "node:fs/promises";
+import { getPathCurrent, triggerErrorFs } from "../helpers/index.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathHashFile = getPathCurrent(
+    import.meta.url,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+  try {
+    const text = await readFile(pathHashFile, "utf-8").catch((err) =>
+      triggerErrorFs(err)
+    );
+    const hashText = createHash("sha256").update(text).digest("hex");
+    console.log(hashText);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 await calculateHash();
