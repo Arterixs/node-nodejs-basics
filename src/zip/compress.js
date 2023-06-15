e14f3cc7d5353f6d87b9af3f2990d865c72a1101
Arterixs <artemontugg@gmail.com>
2023-06-15 20:18:02 +0300

    feat: add compress task zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { createGzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import { getPathCurrent } from "../helpers/index.js";
+
 const compress = async () => {
-    // Write your code here 
+  const pathOldFile = getPathCurrent(
+    import.meta.url,
+    "files",
+    "fileToCompress.txt"
+  );
+  const pathGzip = getPathCurrent(import.meta.url, "files", "archive.gz");
+  const readFile = createReadStream(pathOldFile);
+  const zipFile = createGzip();
+  const writeFile = createWriteStream(pathGzip);
+  pipeline(readFile, zipFile, writeFile);
 };
 
 await compress();
